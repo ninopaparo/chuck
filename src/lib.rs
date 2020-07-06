2dce13b3865897fe3e07f31bbaf5f6d98daebfb8
Luca Francesca <luca@lucafrancesca.me>
2020-07-06 20:17:34 +0100

    Refactor to use clap and cli args (#3)
    * Refactor to use clap and cli args

--- src/lib.rs
+++ src/lib.rs
@@ -6,26 +6,6 @@ pub mod chuck {
     const RANDOM_URL: &'static str = "/random";
 
     #[derive(Debug, Serialize, Deserialize)]
-    pub enum Category {
-        Animal,
-        Career,
-        Celebrity,
-        Dev,
-        Explicit,
-        Fashion,
-        Food,
-        History,
-        Money,
-        Movie,
-        Music,
-        Political,
-        Religion,
-        Science,
-        Sport,
-        Travel,
-    }
-
-    #[derive(Debug, Serialize, Deserialize)]
     pub struct Response {
         icon_url: String,
         id: String,
@@ -45,37 +25,35 @@ pub mod chuck {
                 cli: reqwest::Client::new(),
             }
         }
-        pub async fn get_random_joke(&self) -> Result<String, reqwest::Error> {
-            let url = format!("{}{}", BASE_URL, RANDOM_URL);
-            Ok(self
-                .cli
-                .get(&url)
-                .send()
-                .await?
-                .json::<self::Response>()
-                .await?
-                .value)
-        }
-        pub async fn get_fact_by_category(&self, c: Category) -> Result<String, reqwest::Error> {
+        pub async fn get_chuck_facts(&self, c: &str) -> Result<String, reqwest::Error> {
             let category: String = match c {
-                Category::Animal => String::from("animal"),
-                Category::Dev => String::from("dev"),
-                Category::Career => String::from("career"),
-                Category::Celebrity => String::from("celebrity"),
-                Category::Explicit => String::from("explicit"),
-                Category::Fashion => String::from("fashion"),
-                Category::Food => String::from("food"),
-                Category::History => String::from("history"),
-                Category::Money => String::from("money"),
-                Category::Movie => String::from("movie"),
-                Category::Music => String::from("music"),
-                Category::Political => String::from("political"),
-                Category::Religion => String::from("religion"),
-                Category::Science => String::from("science"),
-                Category::Sport => String::from("sport"),
-                Category::Travel => String::from("travel"),
+                "Animal" => String::from("animal"),
+                "Dev" => String::from("dev"),
+                "Career" => String::from("career"),
+                "Celebrity" => String::from("celebrity"),
+                "Explicit" => String::from("explicit"),
+                "Fashion" => String::from("fashion"),
+                "Food" => String::from("food"),
+                "History" => String::from("history"),
+                "Money" => String::from("money"),
+                "Movie" => String::from("movie"),
+                "Music" => String::from("music"),
+                "Political" => String::from("political"),
+                "Religion" => String::from("religion"),
+                "Science" => String::from("science"),
+                "Sport" => String::from("sport"),
+                "Travel" => String::from("travel"),
+                _ => String::from("none"),
             };
-            let url = format!("{}{}?category={}", BASE_URL, RANDOM_URL, category);
+
+            let url: String;
+
+            if category == "none" {
+                url = format!("{}{}", BASE_URL, RANDOM_URL);
+            } else {
+                url = format!("{}{}?category={}", BASE_URL, RANDOM_URL, category);
+            }
+
             let res = self
                 .cli
                 .get(&url)
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,17 @@ extern crate reqwest;
 extern crate serde;
 use ::chuck::chuck;
 type CustomResult = Result<(), Box<dyn std::error::Error>>;
+use clap::{load_yaml, App};
+
 #[tokio::main]
 async fn main() -> CustomResult {
-    let r = chuck::Client::new().get_random_joke().await?;
+    let yaml = load_yaml!("args.yml");
+    let matches = App::from(yaml).get_matches();
+
+    let category = matches.value_of("category").unwrap_or("none");
+
+    let r = chuck::Client::new().get_chuck_facts(category).await?;
     println!("{}", r);
-    let r2 = chuck::Client::new()
-        .get_fact_by_category(chuck::Category::Food)
-        .await?;
-    println!("{}", r2);
+
     Ok(())
 }
