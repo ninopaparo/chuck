11f3f09f2e2e2d390d45af8ae7f4239e16e00b8f
Nino Paparo <ninopaparo@users.noreply.github.com>
2020-07-25 13:06:34 +0100

    added interactive prompt (#5)

---
+++ src/utils.rs
@@ -0,0 +1,39 @@
+pub mod utils {
+    use dialoguer::{theme::ColorfulTheme, Select};
+    pub fn show_info() {
+        println!(
+            "{}\nVersion: {}\n",
+            env!("CARGO_PKG_NAME"),
+            env!("CARGO_PKG_VERSION")
+        );
+    }
+    pub fn choose_category() -> String {
+        let selections = &[
+            "Animal",
+            "Career",
+            "Celebrity",
+            "Dev",
+            "Explicit",
+            "Fashion",
+            "Food",
+            "History",
+            "Money",
+            "Movie",
+            "Music",
+            "Political",
+            "Religion",
+            "Science",
+            "Sport",
+            "Travel",
+        ];
+
+        let selection = Select::with_theme(&ColorfulTheme::default())
+            .with_prompt("Please select a category")
+            .default(0)
+            .items(&selections[..])
+            .interact()
+            .unwrap();
+        let category = selections[selection];
+        String::from(category)
+    }
+}
